72912b880a559cbea8e3f840af8fca481be04cc4
l33t.xyz <hello@l33t.xyz>
2020-12-29 00:18:11 +0000

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 // next.config.js
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://github.com/vercel/next.js/blob/canary/packages/next/next-server/server/config.ts
 
 
-module.exports = {
-    /* config options here */
-};
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            /* development only config options here */
+            basePath: ''
+        }
+    }
+
+    return {
+        /* config options for all phases except development here */
+        basePath: ''
+    }
+}

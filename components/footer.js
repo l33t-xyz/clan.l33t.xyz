a5d7f13c9e12b726ad3a05babd9040efb72fc71b
l33t.xyz <hello@l33t.xyz>
2021-02-27 22:12:20 -0800

    reset link style

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,8 @@ function Footer({ children, ...props }) {
             </span>
             <br />
             <span>
-                Clan Website Powered by <a href="https://l33t.xyz">l33t.xyz</a>
+                Clan Website Powered by <a href="https://l33t.xyz">l33t.xyz</a>{' '}
+                | <a href="https://github.com/l33t-xyz/clan.l33t.xyz">GitHub</a>
             </span>
         </footer>
     );
